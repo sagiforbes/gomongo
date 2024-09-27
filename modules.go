0b3457e9e000e6bee631eff628792b8230661555
Sagi Forbes <sagi.forbes@myhome.loc>
2024-09-27 21:46:36 +0300

    fixed GenericResult interface

--- modules.go
+++ modules.go
@@ -9,7 +9,6 @@ type GenericResult interface {
 }
 
 type ReadOneResult[T any] struct {
-	GenericResult
 	Found    bool
 	Document T
 	Err      error
@@ -17,67 +16,56 @@ type ReadOneResult[T any] struct {
 }
 
 type ReadManyResult[T any] struct {
-	GenericResult
 	Documents []T
 	Err       error
 }
 
 type ReadStreamResult[T any] struct {
-	GenericResult
 	DocumentStream chan ReadOneResult[T]
 	Err            error
 }
 
 type WriteManyResult struct {
-	GenericResult
 	DbRes mongo.InsertManyResult
 	Err   error
 }
 
 type WriteOneResult struct {
-	GenericResult
 	DbRes *mongo.InsertOneResult
 	Err   error
 }
 
 type UpdateResult struct {
-	GenericResult
 	DbRes *mongo.UpdateResult
 	Err   error
 }
 
 type DeleteResult struct {
-	GenericResult
 	DelCount int64
 	Err      error
 }
 
 type CountResult struct {
-	GenericResult
 	Count int64
 	Err   error
 }
 
 type CommandResult struct {
-	GenericResult
 	DbRes *mongo.SingleResult
 	Err   error
 }
 
 type IndexCreateResult struct {
-	GenericResult
 	IndexName string
 	Err       error
 }
 
 type IndexDropResult struct {
-	GenericResult
 	Doc bson.Raw
 	Err error
 }
 
 type IndexListResult struct {
-	GenericResult
 	Result []interface{}
 	Err    error
 }
